7c0fa12bbf33668ed604c385d0c929c378a223c6
rajanachinnammadevi <rajanachinnammadevi@gmail.com>
2026-04-21 00:56:15 +0530

    assignment 2

---
+++ Assignments/assignment2.ts
@@ -0,0 +1,40 @@
+// A bank evaluates loan applicants based on the following criteria:
+// 1. Credit Score:
+// o If the credit score is above 750, the loan is automatically approved.
+// o If the credit score is between 650 and 750, additional checks are performed.
+// o If the credit score is below 650, the loan is denied.
+// Create common function and then based on below details, print whether user is eligible to get the loan or not
+let customerName: string = "John Doe";
+let creditScore: number = 680;
+let income: number = 55000.0;
+let isEmployed: boolean = true;
+let debtToIncomeRatio: number = 35.0;
+if (creditScore > 750){
+    console.log("The loan is automatically approved");
+}else if(creditScore <= 750 && creditScore > 650){
+       if(income >= 50000.0){
+         if(isEmployed == true){
+            if(debtToIncomeRatio < 40){
+              console.log("The loan is approved")
+            }else{
+                console.log("The loan is denied")
+            }
+         }else{
+            console.log("The loan is denied")
+         }
+       }else {
+        console.log("The loan is denied")
+       }
+}else if(creditScore < 650){
+    console.log("The loan is automatically denied")
+}
+
+
+// 3. Employment Status:
+// o If the customer’s income is at least 50,000, the system checks whether the customer is
+// employed.
+// o If the customer is unemployed, the loan is denied.
+// 4. Debt-to-Income Ratio:
+// o If the customer is employed, the system checks the debt-to-income (DTI) ratio.
+// o If the DTI ratio is less than 40%, the loan is approved.
+// o If the DTI ratio is 40% or greater, the loan is denied.
